be340cadf8d383cdc99699f746ccc309f74f80d9
Gabriel Rosa <gabriel@rosa.dev.br>
2023-05-08 00:28:05 -0300

    about color error fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,5 +8,6 @@ export default {
     extend: {},
   },
   plugins: [],
-  darkMode: 'class'
+  darkMode: 'class',
+  safelist: ['text-cyan-500', 'text-emerald-500', 'text-lime-500', 'text-lime-700', 'text-indigo-500', 'text-orange-600', 'text-rose-600', 'text-sky-500', 'text-purple-600', 'text-violet-500']
 }
