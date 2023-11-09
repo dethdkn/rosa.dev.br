d87e9fba603dc60d7fdad0d5c6b29f80607e8297
Gabriel Rosa <gabriel@rosa.dev.br>
2023-11-08 22:25:54 -0300

    🥚 added an easter egg

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,19 @@
 <script setup lang="ts">
-
+const { x, y } = useMouse()
+const hidden = ref(true)
+function nope() {
+	hidden.value = false
+	setTimeout(() => hidden.value = true, 5000)
+}
 </script>
 
 <template>
-	<NuxtLoadingIndicator color="#0284c7" :height="4" />
-	<Navbar />
-	<main class="min-h-screen ml-[3.35rem] bg-gray-100 dark:bg-gray-900 transition-500">
+	<NuxtLoadingIndicator color="#0284c7" :height="4" @contextmenu.prevent="nope" />
+	<nav @contextmenu.prevent="nope">
+		<Navbar />
+	</nav>
+	<main class="min-h-screen ml-[3.35rem] bg-gray-100 dark:bg-gray-900 transition-500" @contextmenu.prevent="nope">
 		<slot />
 	</main>
+	<img src="/nope.gif" alt="nope emoji gif" class="absolute -translate-x-[75%] -translate-y-1/2 h-32 cursor-none z-50 rounded-md" :class="{ hidden }" :style="{ left: `${x}px`, top: `${y}px` }" @contextmenu.prevent>
 </template>
