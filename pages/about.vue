843e0d7b230284f04ec474d463b5bcdf8e6ec14c
Gabriel Rosa <gabriel@rosa.dev.br>
2023-11-10 22:33:43 -0300

    🥚 added more easter eggs

--- pages/about.vue
+++ pages/about.vue
@@ -10,12 +10,14 @@ defineOgImage({
 useSeoMeta({
 	description: t('about.title'),
 })
+
+const hidden = ref(true)
 </script>
 
 <template>
 	<Section>
 		<Card class="text-center">
-			<img class="w-24 h-24 rounded-full mx-auto" src="/images/gsr.png" :alt="t('about.img-alt')">
+			<img class="w-24 h-24 rounded-full mx-auto cursor-help" src="/images/gsr.png" :alt="t('about.img-alt')" @mouseover="hidden = false" @mouseleave="hidden = true">
 			<p class="dark:text-white m-4">
 				{{ t('about.title') }}
 			</p>
@@ -36,5 +38,6 @@ useSeoMeta({
 				{{ t('about.learner') }}
 			</p>
 		</Card>
+		<img src="/dance.gif" alt="dance boy gif" class="absolute right-0 bottom-0" :class="{ hidden }">
 	</Section>
 </template>
--- pages/experience.vue
+++ pages/experience.vue
@@ -11,12 +11,15 @@ useSeoMeta({
 	description:
 		'Explore my academic and professional journey, showcasing my accomplishments, research, and career milestones, demonstrating the value I bring to your endeavors.',
 })
+
+const hiddenStudy = ref(true)
+const hiddenNotebook = ref(true)
 </script>
 
 <template>
 	<Section>
 		<Card class="md:flex">
-			<div class="mr-6">
+			<div class="mr-6 cursor-help" @mouseover="hiddenStudy = false" @mouseleave="hiddenStudy = true">
 				<Progress
 					:title="t('experience.academic')"
 					:location="t('experience.unicarioca')"
@@ -24,7 +27,7 @@ useSeoMeta({
 					:subtitle="t('experience.cs')"
 				/>
 			</div>
-			<div class="mt-8 md:mt-0">
+			<div class="mt-8 md:mt-0 cursor-help" @mouseover="hiddenNotebook = false" @mouseleave="hiddenNotebook = true">
 				<Progress
 					:title="t('experience.professional')"
 					:location="t('experience.cbpf')"
@@ -33,5 +36,7 @@ useSeoMeta({
 				/>
 			</div>
 		</Card>
+		<img src="/study.gif" alt="studying gif" class="absolute w-64 right-0 bottom-0 transform -scale-x-100" :class="{ hidden: hiddenStudy }">
+		<img src="/notebook.gif" alt="notebook tapping gif" class="absolute right-0 top-0" :class="{ hidden: hiddenNotebook }">
 	</Section>
 </template>
