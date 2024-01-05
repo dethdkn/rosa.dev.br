fb807caed01edc9f2a8a39492a437f1647d664cc
Gabriel Rosa <gabriel@rosa.dev.br>
2024-01-04 23:48:46 -0300

    💄 modified mail icon

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,14 +1,3 @@
-<script setup lang='ts'>
-const socials = ref([
-	{ url: 'mailto:gabriel@rosa.dev.br', icon: 'iconoir:mail-open', name: 'Email' },
-	{ url: 'https://github.com/dethdkn', icon: 'iconoir:github', name: 'Github' },
-	{ url: 'https://www.linkedin.com/in/gabrielsdrosa', icon: 'iconoir:linkedin', name: 'Linkedin' },
-	{ url: 'https://www.youtube.com/@gabrielsdrosa', icon: 'iconoir:youtube', name: 'Youtube' },
-	{ url: 'https://www.behance.net/dethdkn', icon: 'iconoir:behance', name: 'Behance' },
-	{ url: 'https://www.instagram.com/gabriel.sdrosa', icon: 'iconoir:instagram', name: 'Instagram' },
-])
-</script>
-
 <template>
 	<footer class="bg-[#EFF1F5] dark:bg-[#1E1E2E] space-y-4 pt-20 pb-10">
 		<div class="grid grid-cols-3 sm:grid-cols-6 gap-4 place-items-center lg:flex lg:justify-center lg:items-center lg:space-x-8">
@@ -17,7 +6,7 @@ const socials = ref([
 			</NuxtLink>
 		</div>
 		<div class="flex flex-col justify-center items-center text-[#4C4F69] dark:text-[#CDD5F4] space-y-2">
-			<p>© {{ new Date().getFullYear() }} — Gabriel 'DethDKN' Rosa</p>
+			<p><Icon name="iconoir:copyright" class="mb-1" size="18" /> {{ new Date().getFullYear() }} <span class="mx-2">—</span> Gabriel 'DethDKN' Rosa</p>
 			<p>
 				Powered by&nbsp;
 				<NuxtLink to="https://nuxt.com" external target="_blank" class="text-[#43DD7D] transition-all duration-300 nuxt">
---
+++ composables/socials.ts
@@ -0,0 +1,8 @@
+export default ref([
+	{ url: 'mailto:gabriel@rosa.dev.br', icon: 'iconoir:at-sign', name: 'Email' },
+	{ url: 'https://github.com/dethdkn', icon: 'iconoir:github', name: 'Github' },
+	{ url: 'https://www.linkedin.com/in/gabrielsdrosa', icon: 'iconoir:linkedin', name: 'Linkedin' },
+	{ url: 'https://www.youtube.com/@gabrielsdrosa', icon: 'iconoir:youtube', name: 'Youtube' },
+	{ url: 'https://www.behance.net/dethdkn', icon: 'iconoir:behance', name: 'Behance' },
+	{ url: 'https://www.instagram.com/gabriel.sdrosa', icon: 'iconoir:instagram', name: 'Instagram' },
+])
