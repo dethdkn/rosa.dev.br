087cfb22677c1bfcfff549e489d22ba9d1edf7e3
Gabriel Rosa <gabriel@rosa.dev.br>
2024-01-27 21:34:07 -0300

    🎨 Switched to the correct error handling func

--- error.vue
+++ error.vue
@@ -1,14 +1,22 @@
 <script setup lang='ts'>
-const error = defineProps(['error'])
-const { statusCode, message } = error.error
+const error = useError()
+
+const statusCode = ref(500)
+const message = ref('Something went wrong...')
+
+if (error.value && 'statusCode' in error.value && 'message' in error.value) {
+	statusCode.value = error.value.statusCode
+	message.value = error.value.message
+}
+
 useHead({
-	title: `${statusCode}`,
+	title: `${statusCode.value}`,
 })
 </script>
 
 <template>
 	<NuxtLayout>
-		<div class="min-h-[calc(100vh-273px)] flex items-center justify-center">
+		<div class="min-h-[calc(100vh-305px)] flex items-center justify-center">
 			<div class="text-center space-y-10">
 				<h1 class="text-[#4C4F69] dark:text-[#CDD5F4] text-8xl border-b-2 border-[#F28AA9] inline">
 					{{ statusCode }}
