0fd2edd2d89da691ca717ad41ac088cdb95d5992
Gabriel Rosa <gabriel@rosa.dev.br>
2025-07-28 13:19:02 -0300

    🎨 Moved redirect to proxy

--- server/routes/certificates/[...slug].ts
+++ server/routes/certificates/[...slug].ts
@@ -4,5 +4,5 @@ export default defineEventHandler(async event => {
   const file = await head(`certificates/${getRouterParam(event, 'slug')}`, { token: blobReadWriteToken })
     .catch(() => { throw createError({ statusCode: 404, message: 'Certificate not found' }) })
 
-  return sendRedirect(event, file.url, 302)
+  return sendProxy(event, file.url)
 })
--- server/routes/downloads/[...slug].ts
+++ server/routes/downloads/[...slug].ts
@@ -4,5 +4,5 @@ export default defineEventHandler(async event => {
   const file = await head(`downloads/${getRouterParam(event, 'slug')}`, { token: blobReadWriteToken })
     .catch(() => { throw createError({ statusCode: 404, message: 'File not found' }) })
 
-  return sendRedirect(event, file.url, 302)
+  return sendProxy(event, file.url)
 })
