54b299b9de35ca7b03d6c7f99c8090176083802c
Gabriel Rosa <gabriel@rosa.dev.br>
2025-08-18 11:25:00 -0300

    ✨ Add ldap nel

--- server/api/projects.ts
+++ server/api/projects.ts
@@ -1,7 +1,7 @@
 export default defineEventHandler(async event => {
   const t = await useTranslation(event)
 
-  const projects = (await mongo.collection('projects').find<Projects>({}).toArray()).map(p => ({ ...p, description: t(p.description) }))
+  const projects = (await mongo.collection('projects').find<Projects>({}).toArray()).map(p => ({ ...p, description: t(p.description) })).toReversed()
 
   return projects
 })
