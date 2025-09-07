938123213a3a577c261c3ec2db707fbd6d87bc61
Gabriel Rosa <gabriel@rosa.dev.br>
2025-09-07 19:08:31 -0300

    🐛 Order certificates

--- server/api/certificates.ts
+++ server/api/certificates.ts
@@ -1,5 +1,16 @@
 export default defineEventHandler(async () => {
   const certificates = await mongo.collection('certificates').find<Certificates>({}).toArray()
 
+  certificates.sort((a: Certificates, b: Certificates) => {
+    const [am, ay] = a.concluded.split('/')
+    const [bm, by] = b.concluded.split('/')
+
+    const ak = (Number(ay) || Infinity) * 100 + (Number(am) || 0)
+    const bk = (Number(by) || Infinity) * 100 + (Number(bm) || 0)
+
+    if(ak !== bk) return ak - bk
+    return a.title.localeCompare(b.title)
+  })
+
   return certificates
 })
