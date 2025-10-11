9b0e48767892c30fd7ff95d1a62bdbd168cef235
Gabriel Rosa <gabriel@rosa.dev.br>
2025-10-11 16:41:33 -0300

    🐛 Fixed return name

--- server/routes/certificates/[...slug].get.ts
+++ server/routes/certificates/[...slug].get.ts
@@ -1,10 +1,10 @@
 export default defineEventHandler(async event => {
   const name = `certificates/${getRouterParam(event, 'slug')?.replaceAll('/', '') || ''}`
 
-  const { file, headers } = await r2(name)
+  const { buffer, headers } = await r2(name)
     .catch(() => { throw createError({ statusCode: 404, statusMessage: 'Certificate Not Found' }) })
 
   appendHeaders(event, headers)
 
-  return file
+  return buffer
 })
--- server/routes/downloads/[...slug].get.ts
+++ server/routes/downloads/[...slug].get.ts
@@ -1,10 +1,10 @@
 export default defineEventHandler(async event => {
   const name = `downloads/${getRouterParam(event, 'slug')?.replaceAll('/', '') || ''}`
 
-  const { file, headers } = await r2(name)
+  const { buffer, headers } = await r2(name)
     .catch(() => { throw createError({ statusCode: 404, statusMessage: 'File Not Found' }) })
 
   appendHeaders(event, headers)
 
-  return file
+  return buffer
 })
