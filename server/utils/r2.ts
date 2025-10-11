4bd8322939e607b2a5ccb2ed119e29560910a3c1
Gabriel Rosa <gabriel@rosa.dev.br>
2025-10-11 16:18:15 -0300

    🧪 Remove catch

--- server/utils/r2.ts
+++ server/utils/r2.ts
@@ -1,7 +1,7 @@
 const R2 = createStorage({ driver: R2Driver({ binding: 'R2' }) })
 
 export default async function(filePath: string){
-  const file = await R2.getItemRaw<Uint8Array>(filePath, { type: 'bytes' }).catch(() => null)
+  const file = await R2.getItemRaw<Uint8Array>(filePath, { type: 'bytes' })
 
   if(!file) throw new Error('File not found')
 
