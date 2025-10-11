be265f8769987d37c372dc941dc487b050531d29
Gabriel Rosa <gabriel@rosa.dev.br>
2025-10-11 15:25:25 -0300

    🎨 Added observability

--- server/routes/certificates/[...slug].get.ts
+++ server/routes/certificates/[...slug].get.ts
@@ -2,6 +2,7 @@ export default defineEventHandler(async event => {
   const name = `certificates/${getRouterParam(event, 'slug')}`
 
   const { buffer, headers } = await r2(name)
+    .catch(() => { throw createError({ statusCode: 404, statusMessage: 'Certificate Not Found' }) })
 
   appendHeaders(event, headers)
 
--- server/routes/downloads/[...slug].get.ts
+++ server/routes/downloads/[...slug].get.ts
@@ -2,6 +2,7 @@ export default defineEventHandler(async event => {
   const name = `downloads/${getRouterParam(event, 'slug')}`
 
   const { buffer, headers } = await r2(name)
+    .catch(() => { throw createError({ statusCode: 404, statusMessage: 'File Not Found' }) })
 
   appendHeaders(event, headers)
 
