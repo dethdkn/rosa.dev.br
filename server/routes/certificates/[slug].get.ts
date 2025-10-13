690711689651709c3eeeea537f66b1c3a770eaaa
Gabriel Rosa <gabriel@rosa.dev.br>
2025-10-13 10:59:00 -0300

    🚧 Debug

--- server/routes/certificates/[slug].get.ts
+++ server/routes/certificates/[slug].get.ts
@@ -1,5 +1,5 @@
 export default defineEventHandler(async event => {
-  const name = `certificates/${getRouterParam(event, 'slug') || ''}`
+  const name = `certificates:${getRouterParam(event, 'slug') || ''}`
 
   const { file, headers } = await r2(name)
     .catch(error => { throw createError({ statusCode: 404, statusMessage: `Certificate Not Found ${String(error)}` }) })
